416d169b51d50f90cf1813b5b5849316998eefd5
Sergey <wiggo91@mail.ru>
2023-09-16 15:08:14 +0500

    add  Encoder.Menu

---
+++ src/main/java/Menu.java
@@ -0,0 +1,30 @@
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("""
+                    Выберите действие , введя его номер.
+                    1.Зашифровать текст
+                    2.Расшифровать текст
+                    3.Подобрать ключ
+                    4.Синтаксический анализ
+                    5.Закрыть программу""");
+
+            String answer = String.valueOf(Util.readInt());
+
+            switch (answer) {
+                case "1" -> new EncryptedDecrypted().encryptedDecrypted(true);
+                case "2" -> new EncryptedDecrypted().encryptedDecrypted(false);
+                case "3" -> new Bruteforce().bruteforce();
+                case "4" -> System.out.println("Синтаксический анализ");
+                case "5" -> {
+                    return;
+                }
+            }
+
+        }
+    }
+}
